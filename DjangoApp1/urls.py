371c127e9b766341a077961fc81891f51f54252e
fan pu <pufan218@gmail.com>
2020-07-15 01:50:43 -0700

    Display Static Page

--- DjangoApp1/urls.py
+++ DjangoApp1/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from DjangoApp1.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('DjangoApp1/',include('DjangoApp1.urls')),
+    path('', HelloWorld.as_view(), name='helloworld')
 ]
